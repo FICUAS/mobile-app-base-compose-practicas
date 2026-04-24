806111b10fb2170c76ab7383d9ad271b54fafb0b
Instructor FIC <instructorfic@gmail.com>
2026-04-23 19:15:03 -0700

    feat: agregar nueva pantalla HomeScreen

--- app/src/main/java/com/fic/mobile_app_base_compose/MainActivity.kt
+++ app/src/main/java/com/fic/mobile_app_base_compose/MainActivity.kt
@@ -5,12 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.fic.mobile_app_base_compose.ui.screens.home.HomeScreen
 import com.fic.mobile_app_base_compose.ui.theme.MobileappbasecomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +17,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             MobileappbasecomposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileappbasecomposeTheme {
-        Greeting("Android")
     }
 }
